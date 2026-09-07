2a171dc111007629494b4f1806dbf6f66e4b847e
Steffi Gmeiner <053818@edu.szf.at>
2026-09-07 12:17:25 +0200

    concert model update, create user model, update scraper, test db connection

--- ConcertsGraz/Models/User.cs
+++ ConcertsGraz/Models/User.cs
@@ -1,6 +1,19 @@
-﻿namespace ConcertsGraz.Models;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ConcertsGraz.Models;
 
 public class User
 {
-    
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = string.Empty;
+
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty; // pw as hash only
+
+    // save bookmarked concerts and attended concerts via string ids in lists
+    public List<string> BookmarkedConcertIds { get; set; } = new();
+    public List<string> AttendedConcertIds { get; set; } = new();
 }
