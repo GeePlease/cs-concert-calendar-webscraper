193607d95d290624fafb8fb1ae46729e5775b68b
Steffi Gmeiner <053818@edu.szf.at>
2026-09-08 17:18:28 +0200

    refactor project structure: ConcertsGrazDatabaseSettings.cs in new Data Folder

--- ConcertsGraz/Utilities/PasswordHasher.cs
+++ ConcertsGraz/Utilities/PasswordHasher.cs
@@ -1,6 +1,24 @@
-﻿namespace ConcertsGraz.Utilities;
+﻿using Microsoft.AspNetCore.Identity;
+using ConcertsGraz.Models;
+
+namespace ConcertsGraz.Utilities;
 
 public class PasswordHasher
 {
+    // ATTRIBUTES
+    // official ASP.NET Core PasswordHasher
+    private static readonly PasswordHasher<object> _passwordHasher = new PasswordHasher<object>();
+
+    // CONSTRUCTOR
     
+    // METHODS
+    // pw hashing
+    public static string HashPassword(string password)
+    {
+        // user hasher
+        return _passwordHasher.HashPassword(null, password);
+    }
+    
+    
+// END CLASS
 }
