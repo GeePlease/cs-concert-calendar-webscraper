61da32c91fa73b0cb54318524997c152427b5740
Steffi Gmeiner <053818@edu.szf.at>
2026-09-09 13:01:35 +0200

    refactor TextCleaner

--- ConcertsGraz/Utilities/TextCleaner.cs
+++ ConcertsGraz/Utilities/TextCleaner.cs
@@ -5,7 +5,7 @@
 
 using System.Text.RegularExpressions;
 using HtmlAgilityPack;
-namespace ConcertsGraz.Utilites;
+namespace ConcertsGraz.Utilities;
 
 public static class TextCleaner
 {
@@ -14,37 +14,55 @@ public static class TextCleaner
     // CONSTRUCTOR
     // METHODS
     
-    // 1- global cleaner (title, venue, date, time, price) - nullable string
+    // 1 global cleaner (title, venue, date, time, price) - nullable string
     public static string CleanText(string? rawSingleString)
     {
-        // null/whitespace check
+        // 1.1 null/whitespace check
         if (string.IsNullOrWhiteSpace(rawSingleString)) {return"";}
         
-        // DeEntizize HTML special chars (&amp, &nbsp, etc)
+        // 1.2 DeEntizize HTML special chars (&amp, &nbsp, etc)
         string deEntSingleString = HtmlEntity.DeEntitize(rawSingleString);
         
-        // manage empty space ( tab, new line, etc. into 1 whitespace only)
+        // 1.3 manage empty space ( tab, new line, etc. into 1 whitespace only)
         string cleanString = Regex.Replace(deEntSingleString, @"\s+", " ").Trim();
         
-        // return basic cleaned string
+        //1.4 return basic cleaned string
         return cleanString;
     }
 
     
     
-    // 2- description cleaner (description variable) - nullable string
+    // 2 description cleaner (description variable) - nullable string
     public static string CleanDescription(string? rawMultiString)
     { 
-        //HTML-Tags via HtmlAgilityPack strippen (InnerText)
-        //DeEntitize
-        //Emojis / Sonderzeichen entfernen
-        //URLs in description text entfernen
-        //Trimming von eckigen Klammern am Textende ([...])
-        //manage empty space (tabs etc. plus trim)
+        // 2.0 null/whitespace check
+        if (string.IsNullOrWhiteSpace(rawMultiString)) {return"";}
+        
+        // 2.1 <p> / <br> durch Leerzeichen ersetzen (verhindert Wortverklebungen)
+        string cleanedString = Regex.Replace(rawMultiString, @"</p>|<br\s*/?>", " ", RegexOptions.IgnoreCase);
+        
+        // 2.2 HTML-Tags via HtmlAgilityPack strippen (InnerText)
+        var tempNode = HtmlNode.CreateNode(cleanedString);
+        if (tempNode != null) { cleanedString = tempNode.InnerText; } // if node exists, text only
+        else { cleanedString = ""; } //if empty, empty string
+        
+        // 2.3 DeEntitize
+        cleanedString= HtmlEntity.DeEntitize(cleanedString); // deentizize (translate HTML characters back to normal like &amp)
+        
+        // 2.4 Emojis / Sonderzeichen entfernen
+        cleanedString = Regex.Replace(cleanedString, @"[\uD800-\uDBFF][\uDC00-\uDFFF]|[\u2600-\u27BF]", " "); //remove emojis and weird chars
+        
+        // 2.5 URLs in description text entfernen
+        cleanedString = Regex.Replace(cleanedString, @"https?://[^\s]+", "").Trim(); // remove urls in descriptoin text
+        
+        // 2.6 Trimming von eckigen Klammern am Textende ([...])
+        cleanedString = Regex.Replace(cleanedString, @"\s*\[([^\]]+)\]\s*$", "..").Trim(); //remove [] at end
+        
+        //2. 7 manage empty space (tabs etc. plus trim)
+        cleanedString = Regex.Replace(cleanedString, @"\s+", " ").Trim(); // remove empty space
 
-        return "";
+        return cleanedString;
     }
-    
     
     
     /*// 3 - scraper-specialized logic IN SCRAPERS not in TextCleaner!:
@@ -54,5 +72,5 @@ public static class TextCleaner
 */
     
     
-    
+// END CLASS
 }
