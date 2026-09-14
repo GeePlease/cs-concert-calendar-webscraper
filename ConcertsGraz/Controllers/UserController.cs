aa95dc2d90290389a8122c2dcbbae0c3685bb948
Steffi Gmeiner <053818@edu.szf.at>
2026-09-14 09:27:01 +0200

    plan user service, plan user controller even more

--- ConcertsGraz/Controllers/UserController.cs
+++ ConcertsGraz/Controllers/UserController.cs
@@ -1,6 +1,16 @@
-﻿namespace ConcertsGraz.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
 
-public class UserController
+namespace ConcertsGraz.Controllers;
+
+public class UserController : ControllerBase
 {
+    
+    // ATTRIBUTES
+    // CONSTRUCTOR: di - user service
+    
+    // API GET (Single user by name or email)
+    // API PUT (Update single user profile name or email)
+    // API PUT (update password)
+    // API DELETE (delete user by id)
     
 }
--- ConcertsGraz/Services/UserService.cs
+++ ConcertsGraz/Services/UserService.cs
@@ -2,5 +2,35 @@
 
 public class UserService
 {
+    // ATTRIBUTES
+    // CONSTRUCTOR
+    // METHODS
     
+    // GET SINGLE USER FROM DB BY USERNAME
+    // nullcheck
+    // search by username in db
+    // return user object (if found, else null)
+    
+    // GET SINGLE USER FROM DB BY EMAIL
+    // nullcheck
+    // search by email in db
+    // return user object (if found, else null)
+    
+    // UPDATE USER PROFILE - Username (userId, newUsername, newEmail)
+    // search user by...id? (best?) search or nullcheck 
+    // update found user.username with input string // TODO: validator
+    // manage error/success
+    
+    // UPDATE USER - Password (string input)
+    // search user by...id? (best?) search or nullcheck 
+    // update found user.password with input string --> USER HASHER! // TODO: validator
+    // manage error/success
+    
+    // DELETE USER 
+    // search by id? (best? ) search? nullcheck necessary when loggind in
+    // access db and delete user object
+    // mange error/ success
+    
+    
+//END CLASS
 }
