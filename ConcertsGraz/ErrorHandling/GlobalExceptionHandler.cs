01f2064d3b052ec88f261c889ef8476ea80c30e8
Steffi Gmeiner <053818@edu.szf.at>
2026-09-16 13:13:52 +0200

    plan exception handler

---
+++ ConcertsGraz/ErrorHandling/GlobalExceptionHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace ConcertsGraz.ErrorHandling;
+
+public class GlobalExceptionHandler
+{
+    // ATTRIBUTES
+
+    // CONSTRUCTOR
+
+    // METHODS
+
+    // --- API / HTTP EXCEPTIONS
+    // Exceptions related to HTTP requests / API processing
+
+    // --- DATABASE EXCEPTIONS
+    // Exceptions caused by MongoDB / database access
+
+    // --- GENERAL EXCEPTIONS
+    // Unexpected exceptions that don't fit a specific category
+}
