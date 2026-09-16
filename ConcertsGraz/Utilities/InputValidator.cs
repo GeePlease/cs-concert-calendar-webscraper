4c55682b5cfe906e06336cbe8d8e0c17805b9ce4
Steffi Gmeiner <053818@edu.szf.at>
2026-09-16 11:24:08 +0200

    plan input validator

--- ConcertsGraz/Utilities/InputValidator.cs
+++ ConcertsGraz/Utilities/InputValidator.cs
@@ -2,5 +2,20 @@
 
 public class InputValidator
 {
+    // ATTRIBUTES
+    // CONSTRUCTOR
+    // METHODS
     
+    // method suitable for username validation
+    // requ: null check, string len between 3 - 50, trim!
+    
+    // method suitable for email validation
+    // requ: null check, email format  regex at + global, trim!
+    
+    // method suitable for pw validation
+    // requ: null check, string len between 8-50, min 1 zahl (impliziert !null), trim!
+    
+    
+
+//END CLASS
 }
